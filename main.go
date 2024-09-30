e58cb2416c7ce46c9c782f7b0462e3df242de405
Richard Wilkes <wilkes@me.com>
2024-09-30 14:05:50 -0700

    Make the new method of parsing the default

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	cmdline.AppName = "Swagger Doc"
-	cmdline.AppVersion = "2.3.2"
+	cmdline.AppVersion = "2.3.3"
 	cmdline.CopyrightStartYear = "2019"
 	cmdline.CopyrightHolder = "Richard A. Wilkes"
 
@@ -29,31 +29,31 @@ func main() {
 	title := ""
 	serverURL := ""
 	embedded := false
-	parseGoList := false
+	useOldMethod := false
 	var exclude []string
 	cl.NewGeneralOption(&searchDir).SetSingle('s').SetName("search").SetArg("dir").SetUsage("The directory root to search for documentation directives")
 	cl.NewGeneralOption(&mainAPIFile).SetSingle('m').SetName("main").SetArg("file").SetUsage("The Go file to search for the main documentation directives")
 	cl.NewGeneralOption(&destDir).SetSingle('o').SetName("output").SetArg("dir").SetUsage("The destination directory to write the documentation files to")
 	cl.NewGeneralOption(&apiDir).SetSingle('a').SetName("api").SetArg("dir").SetUsage("The intermediate directory within the output directory to write the files to")
 	cl.NewGeneralOption(&baseName).SetSingle('n').SetName("name").SetArg("name").SetUsage("The base name to use for the definition files")
-	cl.NewGeneralOption(&maxDependencyDepth).SetSingle('d').SetName("depth").SetUsage("The maximum depth to resolve dependencies; use 0 for unlimited")
+	cl.NewGeneralOption(&maxDependencyDepth).SetSingle('d').SetName("depth").SetUsage("The maximum depth to resolve dependencies; use 0 for unlimited (only used if --old-method is set)")
 	cl.NewGeneralOption(&markdownFileDir).SetSingle('i').SetName("mdincludes").SetArg("dir").SetUsage("The directory root to search for markdown includes")
 	cl.NewGeneralOption(&title).SetSingle('t').SetName("title").SetArg("text").SetUsage("The title for the HTML page. If unset, defaults to the base name")
 	cl.NewGeneralOption(&serverURL).SetSingle('u').SetName("url").SetArg("url").SetUsage("An additional server URL")
 	cl.NewGeneralOption(&embedded).SetSingle('e').SetName("embedded").SetUsage("When set, embeds the spec directly in the html")
-	cl.NewGeneralOption(&parseGoList).SetSingle('l').SetName("parsegolist").SetUsage("Parse dependencies via 'go list'")
+	cl.NewGeneralOption(&useOldMethod).SetName("old-method").SetUsage("Use old method for parsing dependencies")
 	cl.NewGeneralOption(&exclude).SetSingle('x').SetName("exclude").SetUsage("Exclude directories and files when searching. Example for multiple: -x file1 -x file2")
 	cl.Parse(os.Args[1:])
 	if title == "" {
 		title = baseName
 	}
-	if err := generate(searchDir, mainAPIFile, destDir, apiDir, baseName, title, serverURL, markdownFileDir, exclude, maxDependencyDepth, embedded, parseGoList); err != nil {
+	if err := generate(searchDir, mainAPIFile, destDir, apiDir, baseName, title, serverURL, markdownFileDir, exclude, maxDependencyDepth, embedded, useOldMethod); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func generate(searchDir, mainAPIFile, destDir, apiDir, baseName, title, serverURL, markdownFileDir string, exclude []string, maxDependencyDepth int, embedded, parseGoList bool) error {
+func generate(searchDir, mainAPIFile, destDir, apiDir, baseName, title, serverURL, markdownFileDir string, exclude []string, maxDependencyDepth int, embedded, useOldMethod bool) error {
 	if err := os.MkdirAll(filepath.Join(destDir, apiDir), 0o755); err != nil { //nolint:gosec // Yes, I want these permissions
 		return errs.Wrap(err)
 	}
@@ -65,7 +65,7 @@ func generate(searchDir, mainAPIFile, destDir, apiDir, baseName, title, serverUR
 	if markdownFileDir != "" {
 		opts = append(opts, swag.SetMarkdownFileDirectory(markdownFileDir))
 	}
-	opts = append(opts, swag.ParseUsingGoList(parseGoList))
+	opts = append(opts, swag.ParseUsingGoList(!useOldMethod))
 
 	parser := swag.New(opts...)
 
